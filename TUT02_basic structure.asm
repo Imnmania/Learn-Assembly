fb10b3a9cdefd2948586124ade2a9d6931d1e53d
niloybiswas <niloy.biswas@northsouth.edu>
2019-02-18 23:16:21 +0600

    Basic Structure of a Assembly Code

---
+++ TUT02_basic structure.asm
@@ -0,0 +1,23 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    
+    ; WHERE ALL THE VARIABLES WILL BE DECLARED    
+    
+.CODE
+    ;CODE SEGMENT
+    
+    MAIN PROC ;INT MAIN
+        ;STATEMENT
+        ;STATEMENT
+        
+        
+        MOV AH, 4CH
+        INT 21H
+    MAIN ENDP
+    
+    
+    
+    
+END MAIN
+    ;EXIT(0)
